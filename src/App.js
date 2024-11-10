69fd368c663ba23cefa2eeb1e66c26d07b958d2d
Ádám Szoták <szotakadam15@gmail.com>
2024-11-10 16:04:13 +0100

    create basic user page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('fitness/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>User List</h2>
+              {users.map(user =>
+                <tr key={user.id}>
+                      <td style={{whiteSpace: 'nowrap'}}>{user.name}</td>
+                      <td>{user.address}</td>
+                      <td>{user.phone}</td>
+                      <td>{user.id}</td>
+                </tr>
+              )}
+            </div>
+          </header>
+        </div>
   );
 }
 
